c489fc0688657852a11525cfa912905d4ef9c233
IlyaKuzmichev23 <Ultiil@mail.ru>
2024-08-03 16:51:57 +0300

    Update algorithm_router.py
    add create filters

--- backend/api/routers/algorithm_router.py
+++ backend/api/routers/algorithm_router.py
@@ -1,9 +1,13 @@
+from typing import Optional
+
 import psycopg2
 from fastapi import HTTPException, APIRouter
+from pydantic import BaseModel
 from routers.session import open_conn
 from psycopg2.extras import RealDictCursor
 
 algorithm_router = APIRouter(prefix='/algorithm', tags=['Algorithm'])
+
 
 
 @algorithm_router.get('/get_all_users/', name='Get all users')
@@ -78,7 +82,7 @@ def create_like(user_like_from: int, user_like_to: int) -> dict[str, int | list[
                 new_likes = cursor.fetchone()
             if not new_likes:
                 raise HTTPException(status_code=404, detail="user_like_from/user_like_to not found")
-            return {'new line': new_likes}
+            return {'The like has been set': new_likes}
     except psycopg2.IntegrityError:
         raise HTTPException(status_code=400, detail="The user has already been liked")
     except Exception as ex:
@@ -95,7 +99,7 @@ def create_like(user_dislike_from: int, user_dislike_to: int) -> dict[str, int |
                 new_dislikes = cursor.fetchone()
             if not new_dislikes:
                 raise HTTPException(status_code=404, detail="user_dislike_from/user_dislike_to not found")
-            return {'new line': new_dislikes}
+            return {'The dislike has been set': new_dislikes}
     except psycopg2.IntegrityError:
         raise HTTPException(status_code=400, detail="The user has already been disliked")
     except Exception as ex:
@@ -130,42 +134,42 @@ def list_questionnaires(user_id_var: int) -> list[int]:
         with open_conn() as connection:
             with connection.cursor() as cursor:
                 first_request = ("WITH tmp_interests AS( "
-                                "SELECT ui.user_id as id FROM "
-                                "( "
-                                "filters_interests as fi JOIN users_interests AS ui "
-                                "ON fi.interest_id = ui.interest_id "
-                                ")"
-                                "WHERE fi.user_id = %s AND ui.user_id <> %s), "
-                                " "
-                                "tmp_table AS "
-                                "( "
-                                "(SELECT id FROM users WHERE city = (SELECT city FROM filters WHERE user_id = %s) AND "
-                                "id <> %s) "
-                                " UNION ALL "
-                                "(SELECT id FROM users WHERE gender_id = (SELECT gender_id FROM filters WHERE user_id "
-                                "= %s) AND id <> %s) "
-                                " UNION ALL "
-                                "(SELECT id FROM users WHERE target_id = (SELECT target_id FROM filters WHERE user_id "
-                                "= %s) AND id <> %s) "
-                                " UNION ALL "
-                                "(SELECT id FROM users WHERE communication_id = (SELECT communication_id FROM filters "
-                                "WHERE user_id = %s) AND id <> %s) "
-                                " UNION ALL "
-                                "(SELECT id FROM users WHERE height BETWEEN (SELECT height_min FROM filters WHERE "
-                                "user_id = %s) AND (SELECT height_max FROM filters WHERE user_id = %s) AND id <> %s) "
-                                " UNION ALL "
-                                "(SELECT id FROM users WHERE date_part('YEAR', AGE(DATE(birthday))) BETWEEN (SELECT "
-                                "age_min FROM filters WHERE user_id = %s) AND (SELECT age_max FROM filters WHERE "
-                                "user_id = %s) AND id <> %s) "
-                                ") "
-                                " "
-                                "SELECT id "
-                                "FROM (SELECT id FROM tmp_interests UNION ALL SELECT id FROM tmp_table) "
-                                "GROUP BY id "
-                                "HAVING id NOT IN (SELECT user_id_to FROM likes WHERE user_id_from = %s) "
-                                "AND id NOT IN (SELECT user_id_to FROM dislikes WHERE user_id_from = %s) "
-                                "ORDER BY count(*) DESC; "
-                                )
+                                 "SELECT ui.user_id as id FROM "
+                                 "( "
+                                 "filters_interests as fi JOIN users_interests AS ui "
+                                 "ON fi.interest_id = ui.interest_id "
+                                 ")"
+                                 "WHERE fi.user_id = %s AND ui.user_id <> %s), "
+                                 " "
+                                 "tmp_table AS "
+                                 "( "
+                                 "(SELECT id FROM users WHERE city = (SELECT city FROM filters WHERE user_id = %s) AND "
+                                 "id <> %s) "
+                                 " UNION ALL "
+                                 "(SELECT id FROM users WHERE gender_id = (SELECT gender_id FROM filters WHERE user_id "
+                                 "= %s) AND id <> %s) "
+                                 " UNION ALL "
+                                 "(SELECT id FROM users WHERE target_id = (SELECT target_id FROM filters WHERE user_id "
+                                 "= %s) AND id <> %s) "
+                                 " UNION ALL "
+                                 "(SELECT id FROM users WHERE communication_id = (SELECT communication_id FROM filters "
+                                 "WHERE user_id = %s) AND id <> %s) "
+                                 " UNION ALL "
+                                 "(SELECT id FROM users WHERE height BETWEEN (SELECT height_min FROM filters WHERE "
+                                 "user_id = %s) AND (SELECT height_max FROM filters WHERE user_id = %s) AND id <> %s) "
+                                 " UNION ALL "
+                                 "(SELECT id FROM users WHERE date_part('YEAR', AGE(DATE(birthday))) BETWEEN (SELECT "
+                                 "age_min FROM filters WHERE user_id = %s) AND (SELECT age_max FROM filters WHERE "
+                                 "user_id = %s) AND id <> %s) "
+                                 ") "
+                                 " "
+                                 "SELECT id "
+                                 "FROM (SELECT id FROM tmp_interests UNION ALL SELECT id FROM tmp_table) "
+                                 "GROUP BY id "
+                                 "HAVING id NOT IN (SELECT user_id_to FROM likes WHERE user_id_from = %s) "
+                                 "AND id NOT IN (SELECT user_id_to FROM dislikes WHERE user_id_from = %s) "
+                                 "ORDER BY count(*) DESC; "
+                                 )
                 sel_vars = (
                     user_id_var, user_id_var, user_id_var, user_id_var, user_id_var, user_id_var, user_id_var,
                     user_id_var,
@@ -188,3 +192,25 @@ def list_questionnaires(user_id_var: int) -> list[int]:
                 return all_questionnaires
     except Exception as ex:
         raise HTTPException(status_code=500, detail=str(ex))
+
+
+@algorithm_router.post('/create_filters/{user_id}',  name="create filters for user")
+def create_filters(user_id: int, age_min: Optional[int] = None, age_max: Optional[int] = None,
+                   height_min: Optional[int] = None, height_max: Optional[int] = None,
+                   communication_id: Optional[int] = None, target_id: Optional[int] = None,
+                   gender_id: Optional[int] = None, city: Optional[str] = None,
+                   interests: Optional[list[int]] = []) -> dict[str,int]:
+    try:
+        with open_conn() as connection:
+            with connection.cursor() as cursor:
+                filters_ins = ("INSERT INTO filters "
+                               "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+                filt_vars = (user_id, age_min, age_max, height_min, height_max, communication_id, target_id,
+                             gender_id, city)
+                cursor.execute(filters_ins, filt_vars)
+                for key in interests:
+                    cursor.execute("INSERT INTO filters_interests VALUES(%s, %s)", (user_id, key))
+                return {"filters have been added successfully": user_id}
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail=str(ex))
+
