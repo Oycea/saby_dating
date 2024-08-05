3ce3a87fb124956cb294c1d0d5ee073b828ee6e1
Mavisqqq <96820947+Mavisqqq@users.noreply.github.com>
2024-08-05 16:20:29 +0300

    Update photos_router.py

--- backend/api/routers/photos_router.py
+++ backend/api/routers/photos_router.py
@@ -64,27 +64,21 @@ def set_profile_photo(photo_id: int, current_user: User = Depends(get_current_us
         )
 
 
-@photos_router.get("/profile_photo/", status_code=status.HTTP_200_OK)
-def get_profile_photo(current_user: User = Depends(get_current_user)):
+def get_profile_image(id: int):
     try:
-        with open_conn() as connection:
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    """
-                    SELECT image FROM users_images WHERE user_id = %s AND is_profile_image = TRUE
-                    """,
-                    (current_user.id,)
-                )
+        with get_database_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT image FROM users_images WHERE user_id = %s AND is_profile_image = TRUE", (id,))
                 profile_image_data = cursor.fetchone()
                 if profile_image_data is None:
-                    raise HTTPException(
-                        status_code=status.HTTP_404_NOT_FOUND,
-                        detail="Фото профиля не установлено"
-                    )
-                return StreamingResponse(BytesIO(profile_image_data[0]), media_type="image/jpeg")
+                    raise HTTPException(status_code=404, detail="Фото профиля не найдено")
 
+                profile_image_bytes = profile_image_data[
+                    0]  # Преобразование двоичного кода изображения в b64 и передача url
+                image_type = imghdr.what(io.BytesIO(profile_image_bytes))
+                profile_image_base64 = base64.b64encode(profile_image_bytes).decode('utf-8')
+                profile_image = f"data:image/{image_type};base64,{profile_image_base64}"
+
+                return profile_image
     except Exception as ex:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Внутренняя ошибка сервера: {str(ex)}"
-        )
+        raise HTTPException(status_code=500, detail=str(ex))
