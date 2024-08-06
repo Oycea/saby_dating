78ca6bfd5bff06d3d2fe1d9eca1b8711dba0e285
ZXCTRUSIKI <bustmmr221@gmail.com>
2024-08-06 12:02:26 +0300

    added logging + some changes

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
+import logging
+import os
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from routers.event_router import event_router
 from routers.channel_router import channel_router
 from routers.algorithm_router import algorithm_router
 from routers.authorization_router import authorization_router
-from routers.pass_reset_routers import router as pass_reset_router
+from routers.pass_reset_router import pass_reset_router
 from routers.photos_router import photos_router
 
 origins = [
@@ -15,6 +19,24 @@ origins = [
     "http://localhost:8080",
     "http://localhost:8000"
 ]
+
+# Определение пути к директории проекта
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+log_directory = os.path.join(base_dir, 'logs')
+log_file_path = os.path.join(log_directory, 'app.log')
+
+# Создание директории, если она не существует
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+
+# Настройка логгера
+logging.basicConfig(
+    filename=log_file_path,
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 
 def create_app() -> FastAPI:
@@ -47,3 +69,12 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    logger.error(f"HTTPException: {exc.detail} - Path: {request.url.path}")
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail},
+    )
