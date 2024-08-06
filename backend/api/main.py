4d7df8dbb62041560e08de0be3a4d565e3bceeb0
Roman Lerner <lerner.r@mail.ru>
2024-08-06 13:16:00 +0300

    changed CORS policy
    Signed-off-by: Roman Lerner <lerner.r@mail.ru>

--- backend/api/main.py
+++ backend/api/main.py
@@ -53,7 +53,7 @@ def create_app() -> FastAPI:
 
     new_app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
