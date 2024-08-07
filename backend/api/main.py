bc48e6b21fe55b42cf4e99f29e3174616da31195
Roman Lerner <lerner.r@mail.ru>
2024-08-07 17:19:20 +0300

    fixed edit_event_info
    Signed-off-by: Roman Lerner <lerner.r@mail.ru>

--- backend/api/main.py
+++ backend/api/main.py
@@ -57,7 +57,7 @@ def create_app() -> FastAPI:
 
     new_app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
         allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
