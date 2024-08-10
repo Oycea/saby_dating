a1f9286e3535928b2bba363281f3c6696b1bdd33
ZXCTRUSIKI <bustmmr221@gmail.com>
2024-08-10 22:01:55 +0300

    fix password reset

--- backend/api/routers/pass_reset_router.py
+++ backend/api/routers/pass_reset_router.py
@@ -1,23 +1,23 @@
-import os
-
-from fastapi import APIRouter, HTTPException, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-
-from utils import (verify_reset_password_token,is_registrated,change_password,send_message_pass_reset)
+from pydantic import BaseModel
+from fastapi import HTTPException, APIRouter, Body
+from fastapi.responses import JSONResponse
+from utils import (verify_reset_password_token, is_registrated, change_password,
+                   send_message_pass_reset)
 
 pass_reset_router = APIRouter(tags=['Password reset'])
 
-current_file_path = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_file_path, '..', '..', '..'))
-template_dir = os.path.join(project_root, 'frontend/templates')
 
-templates = Jinja2Templates(directory=template_dir)
+class PasswordResetRequest(BaseModel):
+    password: str
+    confirm_password: str
 
 
-@pass_reset_router.get("/reset_password/{token}", response_class=HTMLResponse, name="страница для смены пароля")
-async def reset_password_form(request: Request, token: str):
-    return templates.TemplateResponse("password_reset.html", {"request": request, "token": token})
+@pass_reset_router.get("/validate_token/{token}", name='Проверка токена')
+async def validate_token(token: str):
+    email = verify_reset_password_token(token)
+    if email is None:
+        return JSONResponse(status_code=400, content={"detail": "Invalid token"})
+    return {"email": email}
 
 
 @pass_reset_router.post("/reset_password_by_email/{email}", name='Отправить ссылку для смены пароля на почту')
@@ -25,16 +25,22 @@ async def reset_password(email: str):
     if is_registrated(email):
         token = send_message_pass_reset(email)
         return token
-    raise HTTPException(status_code=404, detail="Вас нет в базе данных")  #Перевести на регистрацию
+    raise HTTPException(status_code=404, detail="Вас нет в базе данных")  # Перевести на регистрацию
 
 
 @pass_reset_router.post("/reset_password/{token}", name='Изменить пароль')
-async def process_reset_password(token: str, password: str,
-                                 confirm_password: str):
+async def process_reset_password(
+        token: str,
+        password_reset: PasswordResetRequest = Body(...),
+):
+    password = password_reset.password
+    confirm_password = password_reset.confirm_password
+
     if password != confirm_password:
         raise HTTPException(status_code=400, detail="Пароли не совпадают")
+
     email = verify_reset_password_token(token)
-    change_password(email, password)
     if email is None:
         raise HTTPException(status_code=400, detail="Invalid token")
+    change_password(email, password)
     return {"message": "Пароль успешно изменен"}
